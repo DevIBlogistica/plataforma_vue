833c0a5a82ddfc734d5c1fda0a3352a2bc06dfc8
DevIBlogistica <desenvolvimento.iblogistica@gmail.com>
2025-01-21 16:04:50 -0300

    Ajustado o layout inicial da ItemPage

--- src/views/ItemPage.vue
+++ src/views/ItemPage.vue
@@ -5,23 +5,20 @@
       <Navbar />
       <div class="content-container">           
         <div class="main-column">
-          <div class="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark flex-1">
-            <div class="p-4">
-              <div class="mb-6 flex flex-wrap gap-5 border-b border-stroke dark:border-strokedark sm:gap-10">
+          <div class="rounded-sm border border-stroke bg-white shadow-default">
+            <div class="px-4 py-2">
+              <div class="tabs-container flex flex-wrap gap-5 border-b border-stroke sm:gap-10">
                 <a aria-current="page" href="/ui-elements/tabs" class="router-link-active router-link-exact-active border-transparent border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base">Profile</a>
                 <a aria-current="page" href="/ui-elements/tabs" class="router-link-active router-link-exact-active border-transparent border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base">Password</a>
                 <a aria-current="page" href="/ui-elements/tabs" class="router-link-active router-link-exact-active border-transparent border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base">Team</a>
                 <a aria-current="page" href="/ui-elements/tabs" class="router-link-active router-link-exact-active text-primary border-primary border-b-2 py-4 text-sm font-medium hover:text-primary md:text-base">Notification</a>
               </div>                  
-                <div class="embed-container">
-                  <iframe class="embed-content" src="https://www.accuweather.com/pt/br/orindi%C3%BAva/36746/weather-radar/36746" allowfullscreen></iframe>
-                </div>
-                </div>
-                </div>
-              </div>
-           
-         
-   
+            </div>
+          </div>
+          <div class="embed-container full-screen">
+            <iframe class="embed-content full-screen" src="https://www.accuweather.com/pt/br/orindi%C3%BAva/36746/weather-radar/36746" allowfullscreen></iframe>
+          </div>
+        </div>
       </div>
       <Ticker />
     </div>
@@ -60,14 +57,30 @@ export default {
   flex-direction: column;
 }
 
+.main-column {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.tabs-container {
+  flex: 0; /* Ensure tabs container does not stretch */
+  border-radius: 5px;
+}
+
 .embed-container {
-  width: 100%;
-  height: 100%;
+  flex: 1;
+  display: flex;
+  
 }
 
 .embed-content {
+  flex: 1;
+  border-radius: 5px;
+}
+
+.full-screen {
   width: 100%;
   height: 100%;
-  border-radius: 5px;
 }
 </style>
